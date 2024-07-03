e1cc6044b41306c5d5e0ba8d1b03ccec7f8882e3
Dustin L <lawson.dustin12@gmail.com>
2024-07-02 19:51:33 -0500

    Creating the counter app

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const [value, setValue] = useState(0);
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <h1>Counter Title</h1>
+      <h2>{value}</h2>
+      <Button onClick={() => setValue((prevState) => prevState + 1)}>Increment</Button>
+      <Button type="button" onClick={() => setValue((prevState) => prevState - 1)}>Decrement</Button>
+      <Button type="button" onClick={() => setValue(0)}>Reset</Button>
+    </>
   );
 }
 
